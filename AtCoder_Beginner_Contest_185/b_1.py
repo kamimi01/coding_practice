946ce752b970abb97365291883d230580cae7a47
kamimi01 <awakaru0122@gmail.com>
2020-12-19 17:52:30 +0900

    Practice

--- AtCoder_Beginner_Contest_185/b_1.py
+++ AtCoder_Beginner_Contest_185/b_1.py
@@ -6,4 +6,21 @@ time = 0
 
 for _ in range(m):
   a, b = map(int, input().split())
-  bat -= a - 
+  bat -= a - time
+  # 0以下になったらNoになる
+  if bat <= 0:
+    print('No')
+    sys.exit(0)
+  # 0以下にならなければ、減らしたバッテリ量にカフェにいる間の充電分を足す。
+  # ただしもし元のバッテリー量を超えたらそれ以上は増えないので、nになる
+  bat = min(n, bat + (b - a))
+  # 前いったカフェを出た時間をtimeに格納する
+  time = b
+
+# 最後に残ったバッテリ量が、bat - (t - time)である
+# それが0以下になった場合はNoになる
+if bat - (t - time) <= 0:
+  print('No')
+# 0以下にならなければYesになる
+else:
+  print('Yes')
--- AtCoder_Beginner_Contest_185/c_1.py
+++ AtCoder_Beginner_Contest_185/c_1.py
@@ -1,2 +1,10 @@
 l = int(input())
 
+ans = 1
+
+for i in range(11):
+  ans *= (l - 1) - i
+for j in range(1, 12):
+  ans //= j
+
+print(ans)
