b762593a8a058117172e321be48a0cd1a9fc3ddd
mika.urakawa <awakaru0122@gmail.com>
2024-01-09 06:43:44 -0800

    add: work someday

---
+++ AtCoder_Beginner_Content_284/a.py
@@ -0,0 +1,4 @@
+n = int(input())
+num_list = [input() for _ in range(n)]
+
+print(num_list)
---
+++ Learn_algorithm_with_python_textbook/lesson1/2.py
@@ -0,0 +1,17 @@
+a = 1
+s = "Python"
+data = "アルゴリズム"
+print(a,s,data)
+
+n = 0
+print("nの初期値は", n)
+n = 20
+print("新たな値",n, "を代入")
+n = n + 80
+print("nに80を足すと", n, "になる")
+n = n - 80
+print("nから100を引くと", n, "になる")
+
+f = 3.14
+s = "Πの値は" + str(f)
+print(s)
