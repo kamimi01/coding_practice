5e81de7898fc360d4eafec902e28e5edf1ea0122
mika.urakawa <awakaru0122@gmail.com>
2024-01-10 15:18:11 -0800

    add: easy calculation

---
+++ learn_algorithm_math_book/2.2.1.py
@@ -0,0 +1,24 @@
+import math
+
+# 四則演算
+print(869 + 120)
+print(869 - 120)
+print(869 * 120)
+print(869 / 120)
+
+# 剰余
+print(8 % 5)
+print(869 % 120)
+
+# 絶対値
+print(abs(-45))
+print(abs(15))
+
+# 累乗
+print(pow(10.0, 2.0))
+print(pow(3.0, 4.0))
+
+# ルート
+print(math.sqrt(4.0))
+print(math.sqrt(2.0))
+
